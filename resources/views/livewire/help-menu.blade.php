7ebe2dda42896869bc0e33c60cb3afedff21933d
Lukas Frey <mail@lukasfrey.cz>
2024-05-04 19:48:58 +0200

    fix: sidebar and topbar visual glitch
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- resources/views/livewire/help-menu.blade.php
+++ resources/views/livewire/help-menu.blade.php
@@ -20,39 +20,41 @@
     <x-filament-actions::modals/>
 
     @push('scripts')
-        @if($hasModalPreviews)
-            @foreach($this->getDocumentation() as $documentable)
-                <x-filament::modal id="{{$documentable->getId()}}"
-                                   :close-by-clicking-away="true"
-                                   :close-button="true"
-                                   width="2xl"
-                                   :slide-over="$hasSlideOverPreviews"
-                                   class="[&_.fi-modal-content]:px-12 [&_.fi-modal-content]:gap-y-0"
-                                   :footer-actions-alignment="\Filament\Support\Enums\Alignment::End"
-                                   :sticky-footer="true"
-                                   :sticky-header="true">
+        <div class="gu-kb-modals h-0 overflow-hidden">
+            @if($hasModalPreviews)
+                @foreach($this->getDocumentation() as $documentable)
+                    <x-filament::modal id="{{$documentable->getId()}}"
+                                       :close-by-clicking-away="true"
+                                       :close-button="true"
+                                       width="2xl"
+                                       :slide-over="$hasSlideOverPreviews"
+                                       class="[&_.fi-modal-content]:px-12 [&_.fi-modal-content]:gap-y-0"
+                                       :footer-actions-alignment="\Filament\Support\Enums\Alignment::End"
+                                       :sticky-footer="true"
+                                       :sticky-header="true">
 
-                    <x-slot name="heading">
-                        @if($hasModalTitleBreadcrumbs && !empty($documentable->getBreadcrumbs()))
-                            {{ KnowledgeBase::breadcrumbs($documentable) }}
-                        @else
-                            {{ $documentable->getTitle() }}
-                        @endif
-                    </x-slot>
-                    {!! $documentable->getSimpleHtml() !!}
-                    <x-slot name="footerActions">
-                        <x-filament::button tag="a"
-                                            :href="$documentable->getUrl()">
-                            {{ __('filament-knowledge-base::translations.open-documentation') }}
-                        </x-filament::button>
-                        <x-filament::button color="gray"
-                                            x-on:click.prevent="$dispatch('close-modal', { id: '{{$documentable->getId()}}' })">
+                        <x-slot name="heading">
+                            @if($hasModalTitleBreadcrumbs && !empty($documentable->getBreadcrumbs()))
+                                {{ KnowledgeBase::breadcrumbs($documentable) }}
+                            @else
+                                {{ $documentable->getTitle() }}
+                            @endif
+                        </x-slot>
+                        {!! $documentable->getSimpleHtml() !!}
+                        <x-slot name="footerActions">
+                            <x-filament::button tag="a"
+                                                :href="$documentable->getUrl()">
+                                {{ __('filament-knowledge-base::translations.open-documentation') }}
+                            </x-filament::button>
+                            <x-filament::button color="gray"
+                                                x-on:click.prevent="$dispatch('close-modal', { id: '{{$documentable->getId()}}' })">
 
-                            {{ __('filament-knowledge-base::translations.close') }}
-                        </x-filament::button>
-                    </x-slot>
-                </x-filament::modal>
-            @endforeach
-        @endif
+                                {{ __('filament-knowledge-base::translations.close') }}
+                            </x-filament::button>
+                        </x-slot>
+                    </x-filament::modal>
+                @endforeach
+            @endif
+        </div>
     @endpush
 </div>
