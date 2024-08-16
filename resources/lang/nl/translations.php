c4de36339d8f650544d200c62c2ecac625736988
ejjaquet-ch <erik-jan@codeholics.io>
2024-08-17 00:02:34 +0200

    Added Dutch translations.

---
+++ resources/lang/nl/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'knowledge-base' => 'Documentatie',
+    'help' => 'Help',
+    'open-documentation' => 'Open documentatie',
+    'close' => 'Sluit',
+    'back-to-default-panel' => 'Terug',
+    'url-copied' => 'De URL is gekopieerd naar je klembord.',
+    'code-copied' => 'De code is gekopieerd naar je klembord.',
+];
