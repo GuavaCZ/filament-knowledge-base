d8e97750915670fef6d5afbc13ac8d18a1536cf1
Sviat <s.fartushnyi@gmail.com>
2024-08-22 15:31:51 +0300

    Added Ukrainian translation

---
+++ resources/lang/uk/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'knowledge-base' => 'Документація',
+    'help' => 'Допомога',
+    'open-documentation' => 'Відкрити документацію',
+    'close' => 'Закрити',
+    'back-to-default-panel' => 'Назад',
+    'url-copied' => 'URL скопійовано у буфер обміну.',
+    'code-copied' => 'Код скопійовано у буфер обміну.',
+];
