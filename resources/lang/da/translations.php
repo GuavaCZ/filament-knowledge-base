6cddfb92d446f0317c7a2a316784d3e62739a509
synsyst <98920660+synsyst@users.noreply.github.com>
2024-11-19 12:08:55 +0100

    Create da translations.php

---
+++ resources/lang/da/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'knowledge-base' => 'Dokumentation',
+    'help' => 'Vejledning',
+    'open-documentation' => 'Åben dokumentation',
+    'close' => 'Luk',
+    'back-to-default-panel' => 'Tilbage',
+    'url-copied' => 'URL blev kopieret.',
+    'code-copied' => 'Kode blev kopieret.',
+];
