feea9396447f89c111505fd83694e14d27f3dab1
Leo Lutz <leo@leolutz.com>
2025-07-24 11:55:31 -0600

    Fix permalink generation to match sidebar and what works

--- src/Markdown/MarkdownRenderer.php
+++ src/Markdown/MarkdownRenderer.php
@@ -73,6 +73,7 @@ class MarkdownRenderer
             ],
             'heading_permalink' => [
                 'id_prefix' => '',
+                'fragment_prefix' => '',
                 'symbol' => $anchorSymbol ?? '',
                 'html_class' => Arr::toCssClasses([
                     'gu-kb-anchor md:absolute md:-left-8 mr-2 md:mr-0 text-primary-600 dark:text-primary-500 font-bold',
