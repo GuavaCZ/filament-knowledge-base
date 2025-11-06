84dae68252f3f07c15fec0bcb08b42dd7ec4448a
Marco Curatitoli <marco.curatitoli@halservice.it>
2025-11-06 08:12:01 +0100

    fixed config file

--- config/filament-knowledge-base.php
+++ config/filament-knowledge-base.php
@@ -2,14 +2,7 @@
 
 // config for Guava/KnowledgeBasePanel
 return [
-    'panel' => [
-        'id' => env('FILAMENT_KB_ID', 'knowledge-base'),
-        'path' => env('FILAMENT_KB_PATH', 'kb'),
-    ],
-
-    'docs-path' => env('FILAMENT_KB_DOCS_PATH', 'docs'),
-
-    'model' => \Guava\FilamentKnowledgeBase\Models\FlatfileNode::class,
+    'flatfile-model' => \Guava\FilamentKnowledgeBase\Models\FlatfileNode::class,
 
     'cache' => [
         'prefix' => env('FILAMENT_KB_CACHE_PREFIX', 'filament_kb_'),
