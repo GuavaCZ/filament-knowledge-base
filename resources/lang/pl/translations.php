21c777767f927e7310972cafe095b8a8dc4bf379
Lukas Frey <mail@lukasfrey.cz>
2026-07-26 16:14:43 +0200

    fix: use the renamed back translation key

--- resources/lang/pl/translations.php
+++ resources/lang/pl/translations.php
@@ -5,7 +5,7 @@ return [
     'help' => 'Pomoc',
     'open-documentation' => 'Otwórz dokumentację',
     'close' => 'Zamknij',
-    'back-to-default-panel' => 'Powrót',
+    'back' => 'Powrót',
     'url-copied' => 'Link został skopiowany do schowka.',
     'code-copied' => 'Kod został skopiowany do schowka.',
 ];
